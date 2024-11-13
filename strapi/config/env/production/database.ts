ceaef2194bd5b6dac8b25bdf20005ed477fe7d16
devvc-cyber <rafael@volvecare.com>
2024-11-13 11:07:11 -0300

    change database configuration

---
+++ strapi/config/env/production/database.ts
@@ -0,0 +1,24 @@
+// path: /config/env/production/database.ts
+import fs from 'fs';
+const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+   process.env.DATABASE_URL
+ );
+
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host,
+      port,
+      database,
+      user,
+      password,
+      ssl: {
+        ca: fs.readFileSync('./config/env/production/ca-certificate.crt').toString(),      
+      },
+    },
+    debug: false,
+  },
+});
